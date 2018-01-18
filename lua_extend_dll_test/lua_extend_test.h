02463599047a959b99628e018c5a269a281b6976
unknown <cz_jjq@163.com>
2018-01-18 21:08:23 +0800

    upgrade

--- lua_extend_dll_test/lua_extend_test.h
+++ lua_extend_dll_test/lua_extend_test.h
@@ -5,12 +5,15 @@
 
 // my_math.c
 
+
+
 /*
 ** Open my_math library
 */
 //LUALIB_API int luaopen_mylib(lua_State *L);
 
 //2018.01.17必须用extern "C" 指定导出
-extern "C" int luaopen_mylib(lua_State *L);
+//extern "C" int luaopen_mylib(lua_State *L);
+extern "C" __declspec(dllexport) int luaopen_mylib(lua_State *L);
 
 #endif // LUA_EXTEND_TEST_H
