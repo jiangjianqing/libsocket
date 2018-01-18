398eecb2d43448fed51be97728644bac1ee70cc6
cz_jjq <cz_jjq@163.com>
2018-01-18 23:30:45 +0800

    upgrade

--- lua_extend_dll_test/lua_extend_test.h
+++ lua_extend_dll_test/lua_extend_test.h
@@ -6,14 +6,28 @@
 // my_math.c
 
 
+#if defined(__linux__)
+// Linux系统
+//#define LUA_API extern "C"
+#elif defined(_WIN32)
 
-/*
-** Open my_math library
-*/
-//LUALIB_API int luaopen_mylib(lua_State *L);
+//#define LUA_API extern "C" __declspec(dllexport)
+// Windows系统
+#endif
 
-//2018.01.17必须用extern "C" 指定导出
+
+#ifdef __cplusplus
+extern "C" { //2018.01.18 c++ compiler 必须用extern "C" 指定导出
+#endif//-----------------------------
+
+
 //extern "C" int luaopen_mylib(lua_State *L);
-extern "C" __declspec(dllexport) int luaopen_mylib(lua_State *L);
+LUALIB_API int luaopen_mylib(lua_State *L);
+
+
+
+#ifdef __cplusplus//-------------------------------------
+}   //end of extern "C" {
+#endif
 
 #endif // LUA_EXTEND_TEST_H
