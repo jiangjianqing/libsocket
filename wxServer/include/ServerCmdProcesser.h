d1fc67eae4f0af9c2c39b3b7d9c3ad1597f00f6f
cz_jjq <cz_jjq@163.com>
2018-01-24 09:09:43 +0800

    upgrade

--- wxServer/include/ServerCmdProcesser.h
+++ wxServer/include/ServerCmdProcesser.h
@@ -4,6 +4,14 @@
 #include "wx/event.h"
 #include "CmdBufParser.h"
 
+enum class CmdEventType{
+    UNKNOW,RecvTcpIdentifyResponse,
+};
+
+#define ID_CMDPROCESSER_THREADEVENT   wxID_HIGHEST+30
+
+typedef function<void (const CmdEventType& event)> CmdEventCb;
+
 class ServerCmdProcesser : public CmdBufParser
 {
 public:
@@ -12,9 +20,12 @@ protected:
     void onRecvCmd(const unsigned char* buf,const unsigned len) override;
 private:
     wxEvtHandler* m_wxEvtHandler;
-    int m_clientId;
+    int m_clientId;//tcp通讯时使用的ID
+    int m_responseId;//客户端发回的ID，用于Server根据ID来区分发送内容
 
     bool m_isRecvingFilePartData;
+
+    void callCmdEventCb(const CmdEventType& event);
 
 };
 
--- wxServer/src/ServerCmdProcesser.cpp
+++ wxServer/src/ServerCmdProcesser.cpp
@@ -1,4 +1,7 @@
 #include "ServerCmdProcesser.h"
+#include "tcp_msg.package.pb.h"
+#include "tcp_msg.cmd.pb.h"
+#include "ProtobufUtils.h"
 
 ServerCmdProcesser::ServerCmdProcesser(wxEvtHandler* evtHandler,int clientId)
     : m_wxEvtHandler(evtHandler),m_clientId(clientId)
@@ -9,5 +12,39 @@ ServerCmdProcesser::ServerCmdProcesser(wxEvtHandler* evtHandler,int clientId)
 
 void ServerCmdProcesser::onRecvCmd(const unsigned char* buf,const unsigned len)
 {
+    //注意线程间通讯
+    cmd_message_s* cmd = (cmd_message_s*)buf;
+    tcp_msg::ProtoPackage pkg;
+    if(pkg.ParseFromArray(cmd->payload,cmd->header.length)){//解析成功
+        Message* msg = ProtobufUtils::createMessage(pkg.type_name());
+        if(msg != nullptr){
+            string a = pkg.type_name();
+            if(strcmp(a.c_str(),"tcp_msg.IdentifyResponse") == 0){
 
+                callCmdEventCb(CmdEventType::RecvTcpIdentifyResponse);
+            }
+
+            delete msg;
+        }
+
+    }else{//解析失败
+
+    }
+}
+
+void ServerCmdProcesser::callCmdEventCb(const CmdEventType& eventType)
+{
+
+    //wxThreadEvent e(wxEVT_COMMAND_THREAD, ID_MY_THREAD_EVENT);
+    //wxThreadEvent e;
+    //wxThreadEvent e(wxEVT_COMMAND_THREAD, wxID_ANY);
+    CmdProcesserThreadEvent event(wxEVT_COMMAND_THREAD);
+    event.SetId((int)eventType);
+    //event.SetId((int)socket_event_type::ConnectionAccept);
+    //ostringstream ostr;
+    //ostr<<ip<<":"<<port;
+    //string info = std::to_string(port);
+    //event.SetString(ostr.str());
+    wxQueueEvent(m_wxEvtHandler,event.Clone());
+    //wxTheApp->QueueEvent(e.Clone());
 }
